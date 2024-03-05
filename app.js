007440462bee946d7878091efd6ced5acfccd154
MiyuKubota <lmasganador@gmail.com>
2024-03-05 20:34:58 -0300

    Trabajo Denis Quintana

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.resolve("./public")));
+
+const port = process.env.PORT || 3030
+
+app.listen(port,() => console.log(`Server corriendo en port ${port}`));
+app.get ("/", (req,res) => {
+    res.sendFile(path.resolve("./views/home.html"));
+})
+app.get ("/register", (req,res) => {
+    res.sendFile(path.resolve("./views/register.html"));
+})
+app.get ("/login", (req,res) => {
+    res.sendFile(path.resolve("./views/login.html"));
+})
